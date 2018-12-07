62137b3e4e3507ea43b8bce2ced13de311e5d56b
MTBriles <44682231+MTBriles@users.noreply.github.com>
2018-12-07 08:11:51 -0500

    Changes were made
    Added an 'anonymize' tab to the top. 
    
    We are .grid now and not pack()

--- Main View.py
+++ Main View.py
@@ -23,20 +23,25 @@ class App(tk.Frame):
         parent.geometry('1200x900')
         parent.configure(bg='gray16')
 
+        #_label = Labels
+        #_label()
         parent.label = Labels(self, bg='gray16')
-        parent.label.pack()
+        parent.label.grid(row=1)
 
         menubar = Menu(self.master)
         self.master.config(menu=menubar)
         devices = Menu(menubar, tearoff=0)
         rules = Menu(menubar, tearoff=0)
+        anonymize = Menu(menubar, tearoff=0)
         help = Menu(menubar, tearoff=0)
         menubar.add_cascade(label='Devices', menu=devices)
         menubar.add_cascade(label='Rules', menu=rules)
+        menubar.add_cascade(label='Anonymize', menu=anonymize)
         menubar.add_cascade(label='Help', menu=help)
         devices.add_command(label='Add/Remove Device', command=Views.Devices)
         rules.add_command(label='Add/Delete Rules', command=Views.Rules)
         rules.add_command(label='Active Rules', command=Views.ActiveRules)
+        anonymize.add_command(label='Anonymizer')
         help.add_command(label='Read Me', command=self.open_readme)
 
 
@@ -47,33 +52,36 @@ class Labels(tk.Label):
 
         self.image = tk.PhotoImage(data=main_image)
         self.label_logo = tk.Label(self, image=self.image)
-        self.label_logo.pack(pady=15)
+        self.label_logo.grid(row=0, column=1, pady=15)
+
+        self.label_empty = Label(self, bg='gray16')
+        self.label_empty.grid(row=2, column=0, padx=125)
 
         self.label = tk.Label(self, text='Server Status: ', bg='gray16', fg='alice blue', font=('', 14))
-        self.label.pack()
+        self.label.grid(row=1, column=1)
 
         self.label_status = tk.Label(self, text='Status', bg='gray16', fg='red', font=('', 12))
-        self.label_status.pack(pady=15)
+        self.label_status.grid(row=2, column=1, pady=15)
 
         self.button_start_server = tk.Button(self, text='Start Server', bg='gray20', fg='alice blue')
-        self.button_start_server.pack(pady=15)
+        self.button_start_server.grid(row=3, column=1, pady=15)
 
         self.button_stop_server = tk.Button(self, text='Stop Server', bg='gray20', fg='alice blue')
-        self.button_stop_server.pack(pady=15)
+        self.button_stop_server.grid(row=4, column=1, pady=15)
 
         self.text_logs = tk.Text(self, bg='gray14', fg='alice blue', borderwidth=2)
-        self.text_logs.pack(pady=10)
+        self.text_logs.grid(row=5, column=1, pady=10)
 
 
 class Main(tk.Frame):
 
     def __init__(self, master, *args, **kwargs):
         tk.Frame.__init__(self, master, *args, **kwargs)
-        self.app = App(master).pack()
+        self.app = App(master).grid()
 
 
 
 if __name__ == '__main__':
     root = tk.Tk()
-    Main(root).pack()
+    Main(root).grid()
     root.mainloop()
