0498e3d62ea10493a2ce74df2b63f8d9a4cb1a68
MTBriles <44682231+MTBriles@users.noreply.github.com>
2018-12-13 15:38:50 -0500

    Update
    Rebuilt the main view.  Its now a cleaner init everything living in one class.  
    
    Features added:
    New Menubar objects Services->Create Service, Remove Service
    Live status of the server
    User prompt when exiting the program.

--- Main View.py
+++ Main View.py
@@ -5,88 +5,110 @@ import pynetdicom3
 import os
 from pydicom.filereader import read_dicomdir
 from tkinter import *
+from tkinter import messagebox
 import image_file
 import Views
 import pyodbc
+import run_services
+from threading import Timer
+import sys
+import time
 
 # assigning the image_file to a global var here in the Main View.py. Its used in the Class Labels
 main_image = image_file.main_image_string
+services = run_services
+var = Tk()
 
 
-class App(tk.Frame):
-    # this is the main window/title screen when the app launches
+def label(self, text,  row, column, font=None, pady=None, sticky=None, columnspan=None, padx=None):
+    _l = tk.Label(self, text=text, bg='gray16', fg='alice blue', font=font)
+    _l.grid(row=row, column=column, pady=pady, sticky=sticky, columnspan=columnspan, padx=padx)
+
+
+def button(self, text, command, row, column, sticky=None, pady=None):
+    _b = tk.Button(self, text=text, command=command, bg='gray14', fg='darkorange1')
+    _b.grid(row=row, column=column, sticky=sticky, pady=pady)
+
+
+class Main(tk.Frame):
 
     def open_readme(self):
         # opens the readme doc
         os.system('ReadMe.txt')
 
-    def __init__(self, parent, *args, **kwargs):
-        """This is the display configs for the main view that loads when app launches.  The  Menubar section has
-        the following notes: each 'add_command' references a Class in the Views.py file."""
-
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        parent.title('DST DICOM Router')
-        parent.geometry('1200x900')
-        parent.configure(bg='gray16')
-
-        parent.label = Labels(self, bg='gray16')
-        parent.label.grid(row=1)
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        master.title('DST DICOM Router')
+        master.geometry('1200x900')
+        master.configure(bg='gray16', highlightbackground='gray16', highlightcolor='gray16')
 
         menubar = Menu(self.master)
         self.master.config(menu=menubar)
         devices = Menu(menubar, tearoff=0)
         rules = Menu(menubar, tearoff=0)
         anonymize = Menu(menubar, tearoff=0)
+        service = Menu(menubar, tearoff=0)
         help = Menu(menubar, tearoff=0)
         menubar.add_cascade(label='Devices', menu=devices)
         menubar.add_cascade(label='Rules', menu=rules)
-        menubar.add_cascade(label='Anonymize', menu=anonymize)
+        menubar.add_cascade(label='Configure', menu=anonymize)
+        menubar.add_cascade(label='Services', menu=service)
         menubar.add_cascade(label='Help', menu=help)
         devices.add_command(label='Add/Remove Device', command=Views.Devices)
-        rules.add_command(label='Add/Delete Rules', command=Views.Rules)
+        rules.add_command(label='Activate/Delete Rules', command=Views.Rules)
         rules.add_command(label='Build Rules', command=Views.BuildRules)
-        anonymize.add_command(label='Anonymize Config', command=Views.Anonymize)
+        anonymize.add_command(label='DSTools Config', command=Views.DSTConfig)
+        service.add_command(label='Create Service', command=services.create_service)
+        service.add_command(label='Remove Service', command=services.remove_service)
         help.add_command(label='Read Me', command=self.open_readme)
 
-
-class Labels(tk.Label):
-
-    def __init__(self, *args, **kwargs):
-        """These are the labels/buttons that populate the main view in the Class App."""
-
-        tk.Label.__init__(self, *args, **kwargs)
-
         self.image = tk.PhotoImage(data=main_image)
-        self.label_logo = tk.Label(self, image=self.image)
+        self.label_logo = tk.Label(master, image=self.image, bg='gray16')
         self.label_logo.grid(row=0, column=1, pady=15)
 
-        self.label_empty = Label(self, bg='gray16')
-        self.label_empty.grid(row=2, column=0, padx=125)
+        label(master, '', 2, 0, padx=125, sticky=tk.NSEW)
+        label(master, 'Server Status: ', 1, 1, font=('', 14))
 
-        self.label = tk.Label(self, text='Server Status: ', bg='gray16', fg='alice blue', font=('', 14))
-        self.label.grid(row=1, column=1)
+        button(master, 'Start Server', services.start_service, 3, 1, pady=15)
+        button(master, 'Stop Server', services.stop_service, 4, 1, pady=15)
 
-        self.label_status = tk.Label(self, text='Status', bg='gray16', fg='red', font=('', 12))
+        self.var = StringVar()
+        self.label_status = tk.Label(master, textvariable=self.var, bg='gray16', fg='red', font=('', 12))
         self.label_status.grid(row=2, column=1, pady=15)
 
-        self.button_start_server = tk.Button(self, text='Start Server', bg='gray20', fg='darkorange1')
-        self.button_start_server.grid(row=3, column=1, pady=15)
-
-        self.button_stop_server = tk.Button(self, text='Stop Server', bg='gray20', fg='darkorange1')
-        self.button_stop_server.grid(row=4, column=1, pady=15)
-
-        self.text_logs = tk.Text(self, bg='gray14', fg='alice blue', borderwidth=2)
+        self.text_logs = tk.Text(master, bg='gray14', fg='alice blue', borderwidth=2, highlightbackground='gray16')
         self.text_logs.grid(row=5, column=1, pady=10)
 
+        self.var = StringVar()
+        self.label_status = tk.Label(master, textvariable=self.var, bg='gray16', fg='red', font=('', 12))
+        self.label_status.grid(row=2, column=1, pady=15)
+        self.check()
 
-class Main(tk.Frame):
+    def check(self):
+        if services.check_service() == 'Not_Installed':
+            self.var.set('***Service not installed***')
+            self.label_status.configure(fg='red')
+            self.label_status.after(1000, self.check)
 
-    def __init__(self, master, *args, **kwargs):
-        tk.Frame.__init__(self, master, *args, **kwargs)
-        self.app = App(master).grid()
+        elif services.check_service() == 'Running':
+            self.var.set('***Running***')
+            self.label_status.configure(fg='green')
+            self.label_status.after(1000, self.check)
+        elif services.check_service() == 'Stopped':
+            self.var.set('***Stopped***')
+            self.label_status.configure(fg='red')
+            self.label_status.after(1000, self.check)
+        else:
+            self.check.quit()
 
 
-if __name__ == '__main__':
-    root = tk.Tk()
-    Main(root).grid()
-    root.mainloop()
+def on_closing():
+    if messagebox.askokcancel('Quit', 'Do you want to quit?'):
+        var.destroy()
+        sys.exit()
+
+
+c = Main(var)
+var.protocol('WM_DELETE_WINDOW', on_closing)
+var.mainloop()
+
