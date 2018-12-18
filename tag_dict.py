4f16b4952d47606d77e6aa0ddeed4daa6fbf0182
MTBriles <44682231+MTBriles@users.noreply.github.com>
2018-12-18 13:44:00 -0500

    Added more items to dict

--- tag_dict.py
+++ tag_dict.py
@@ -2,9 +2,15 @@
 
 # Each dict entry is Tag : (VR, VM, Name, Retired, Keyword)
 DicomDictionary = {
-    '0x00080030': 'Study Time',
-    '0x00080031': 'Series Time'
-
+    '0x00080060': 'Modality',
+    '0x00081030': 'StudyDescription',
+    '0x0008103E': 'SeriesDescription',
+    'No Tag': 'Called AE',
+    '0x00080080': 'InstitutionName',
+    '0x00080050': 'AccessionNumber',
+    '0x00100020': 'PatientID',
+    '0x00101000': 'OtherPatientIDs',
+    '0x00180050': 'SliceThickness'
 }
 
 temp = []
